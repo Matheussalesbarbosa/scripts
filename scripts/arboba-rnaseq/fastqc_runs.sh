0215d35675763346eb385c9f2873b885c76f0236
lpmor22 <laisepaixao@live.com>
2019-08-20 15:07:26 -0300

    arboba-rnaseq 2019-08-20
    minor changes

--- scripts/arboba-rnaseq/fastqc_runs.sh
+++ scripts/arboba-rnaseq/fastqc_runs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-nohup fastqc $HOME/datasets/arboba-rnaseq/run1/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-run1/ --threads 12
-nohup fastqc $HOME/datasets/arboba-rnaseq/run2/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-run2/ --threads 12
-nohup fastqc $HOME/datasets/arboba-rnaseq/run3/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-run3/ --threads 12
-nohup fastqc $HOME/datasets/arboba-rnaseq/run4/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-run4/ --threads 12
-nohup fastqc $HOME/datasets/arboba-rnaseq/run5/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-run5/ --threads 12
+nohup fastqc $HOME/datasets/arboba-rnaseq/run1/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-r1/ --threads 12
+nohup fastqc $HOME/datasets/arboba-rnaseq/run2/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-r2/ --threads 12
+nohup fastqc $HOME/datasets/arboba-rnaseq/run3/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-r3/ --threads 12
+nohup fastqc $HOME/datasets/arboba-rnaseq/run4/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-r4/ --threads 12
+nohup fastqc $HOME/datasets/arboba-rnaseq/run5/*.fastq.gz --outdir $HOME/datasets/arboba-rnaseq/qc/qc-r5/ --threads 12
 #nohup: permite executar o comando em segundo plano
 #*.fastq.gz: seleciona os arquivos *.fastq.gz para a analise
 #--outdir: diretorio para os arquivos de saida da analise (output)
--- scripts/arboba-rnaseq/multiqc_runs.sh
+++ scripts/arboba-rnaseq/multiqc_runs.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd $HOME/datasets/arboba-rnaseq/qc/qc-run1/ && nohup multiqc --fullnames --title ArbovirusBahiaRun1 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-run1/
-cd $HOME/datasets/arboba-rnaseq/qc/qc-run2/ && nohup multiqc --fullnames --title ArbovirusBahiaRun2 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-run2/
-cd $HOME/datasets/arboba-rnaseq/qc/qc-run3/ && nohup multiqc --fullnames --title ArbovirusBahiaRun3 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-run3/
-cd $HOME/datasets/arboba-rnaseq/qc/qc-run4/ && nohup multiqc --fullnames --title ArbovirusBahiaRun4 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-run4/
-cd $HOME/datasets/arboba-rnaseq/qc/qc-run5/ && nohup multiqc --fullnames --title ArbovirusBahiaRun5 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-run5/
+cd $HOME/datasets/arboba-rnaseq/qc/qc-r1/ && nohup multiqc --fullnames --title ArbovirusBahiaRun1 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-r1/
+cd $HOME/datasets/arboba-rnaseq/qc/qc-r2/ && nohup multiqc --fullnames --title ArbovirusBahiaRun2 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-r2/
+cd $HOME/datasets/arboba-rnaseq/qc/qc-r3/ && nohup multiqc --fullnames --title ArbovirusBahiaRun3 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-r3/
+cd $HOME/datasets/arboba-rnaseq/qc/qc-r4/ && nohup multiqc --fullnames --title ArbovirusBahiaRun4 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-r4/
+cd $HOME/datasets/arboba-rnaseq/qc/qc-r5/ && nohup multiqc --fullnames --title ArbovirusBahiaRun5 --interactive --export $HOME/datasets/arboba-rnaseq/qc/qc-r5/
 #nohup: permite executar o comando em segundo plano
 #--fullnames: manter o nome do arquivo que vai ser analisado
 #--title: criar um titulo para o relatorio do multiqc
